58a137c9829fecf57cbf116c1656c011ed536016
Minart <mino.mnz@gmail.com>
2020-09-16 00:21:23 +0200

    Start nuxt CLI

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    init
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  asyncData({ params }) {
+    //init
+  }
+}
+</script>
+
+<style>
+</style>
